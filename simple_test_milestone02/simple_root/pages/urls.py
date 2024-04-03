2b4619ae7ec57124e648f60885db3780e390d3fa
A'Yanna Rouse <ayanna.rouse2@mail.dcu.ie>
2024-04-03 17:46:56 +0100

    Week 2

--- simple_test_milestone02/simple_root/pages/urls.py
+++ simple_test_milestone02/simple_root/pages/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('extra/', views.extra, name = 'extra'),
+    path('about/', views.about, name = 'about'),
     path('', views.index, name='index'),
 ]
--- simple_test_milestone02/simple_root/pages/views.py
+++ simple_test_milestone02/simple_root/pages/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Page
 
 def index(request):
-    return HttpResponse("Hello, this is the home page!")
+	pg = Page.objects.get(permalink='/')
+	return HttpResponse(pg.bodytext)
+
+def about(request):
+    pg = Page.objects.get(permalink='/about')
+    return HttpResponse(pg.bodytext)
+
+def extra(request):
+    pg = Page.objects.get(permalink='/extra')
+    return HttpResponse(pg.bodytext)
